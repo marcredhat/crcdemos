1eef0c638df1bfba7c2e980393c145fec3b20dca
Marc Chisinevski <mchisine@redhat.com>
2019-08-29 21:12:01 -0700

    Update step1.sh

--- scripts/step1.sh
+++ scripts/step1.sh
@@ -7,13 +7,4 @@ chmod +x ./*.sh
 ./prereqs.sh
 
 ./adduser.sh
-su $DEMOUSER
-cd /home/$DEMOUSER
-sudo cp /root/crcdemos/scripts/installgo.sh /home/$DEMOUSER
-sudo chmod +x ./*.sh
-./installgo.sh
-echo "export PATH=$HOME/work/bin:$HOME/go/bin:$PATH"  >> $HOME/.bashrc
-source .bashrc
 
-
-go
