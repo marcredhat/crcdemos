40223a59e31f3fdf8aaef2e7b3c583bf2d76e048
Marc Chisinevski <mchisine@redhat.com>
2019-08-29 22:02:54 -0700

    Update 1.sh

--- scripts/1.sh
+++ scripts/1.sh
@@ -9,13 +9,13 @@ chmod +x *.sh
 ./prereqs.sh
 ./adduser.sh
 
-su $DEMOUSER
-cd /home/$DEMOUSER
-git clone https://github.com/marcredhat/crcdemos.git
-cd crcdemos/scripts
-chmod +x *.sh
 
-cd /home/$DEMOUSER
-wget https://raw.githubusercontent.com/marcredhat/crcdemos/master/scripts/installgo.sh
-source /home/$DEMOUSER/.bashrc
-go
+su $DEMOUSER -c "cd /home/$DEMOUSER"
+su $DEMOUSER -c "git clone https://github.com/marcredhat/crcdemos.git"
+su $DEMOUSER -c "cd crcdemos/scripts"
+su $DEMOUSER -c "chmod +x *.sh"
+
+su $DEMOUSER -c "cd /home/$DEMOUSER"
+su $DEMOUSER -c "wget https://raw.githubusercontent.com/marcredhat/crcdemos/master/scripts/installgo.sh"
+su $DEMOUSER -c "source /home/$DEMOUSER/.bashrc"
+su $DEMOUSER -c "go"
