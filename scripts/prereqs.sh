cfb08a94e50e8ba518f416271ea114ad4bd881c7
Marc Chisinevski <mchisine@redhat.com>
2019-08-30 10:07:39 -0700

    Update prereqs.sh

--- scripts/prereqs.sh
+++ scripts/prereqs.sh
@@ -7,9 +7,9 @@ sudo systemctl enable libvirtd
 #Install Python
 #See https://developers.redhat.com/blog/2019/05/07/what-no-python-in-red-hat-enterprise-linux-8/
 
-yum install @python36
+yum -y install @python36
 
-yum install @python27
+yum -y install @python27
 
 #When you install either (or both) you can easily make /usr/bin/python point to the right place using alternatives --config python
 
