d5ffaf2eb8451ba27fd03e31c3896106f8e141fa
Marc Chisinevski <mchisine@redhat.com>
2019-08-29 21:06:40 -0700

    Update step1.sh

--- scripts/step1.sh
+++ scripts/step1.sh
@@ -1,7 +1,22 @@
 
+rm -rf tmp
+mkdir tmp
+cd tmp
+export DEMOUSER=demo14
+git clone https://github.com/marcredhat/crcdemos.git
+cd crcdemos/scripts
+chmod +x ./*.sh
 
 ./prereqs.sh
+
 ./adduser.sh
-
-
+su $DEMOUSER
+cd /home/$DEMOUSER
+sudo cp /root/crcdemos/scripts/installgo.sh /home/$DEMOUSER
+sudo chmod +x ./*.sh
 ./installgo.sh
+echo "export PATH=$HOME/work/bin:$HOME/go/bin:$PATH"  >> $HOME/.bashrc
+source .bashrc
+
+
+go
