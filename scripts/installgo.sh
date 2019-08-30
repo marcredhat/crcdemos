ca0496d97cae7cf3a354e82414f7110ea8cbc4f9
Marc Chisinevski <mchisine@redhat.com>
2019-08-29 19:28:11 -0700

    Update installgo.sh

--- scripts/installgo.sh
+++ scripts/installgo.sh
@@ -1,4 +1,6 @@
-cd /home/marc
+export HOME=/home/marc
+
+cd $HOME
 
 wget https://dl.google.com/go/go1.12.9.linux-amd64.tar.gz
 
@@ -6,10 +8,12 @@ tar -xzf go1.12.9.linux-amd64.tar.gz
 
 #Add to .bashrc:
 
-echo "export GOROOT=/home/marc/go" >> .bashrc
+echo "export GOROOT=$HOME/go" >> $HOME/.bashrc
 
-echo "export GOPATH=/home/marc/work" >> .bashrc
+echo "export GOPATH=$HOME/work" >> $HOME/.bashrc
 
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH"  >> $HOME/.bashrc
 
 source .bashrc
+
+go
