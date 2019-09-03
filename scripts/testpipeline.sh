bd28937a872a40513637453d99f8ed18d6b38283
Marc Chisinevski <mchisine@redhat.com>
2019-09-03 12:37:28 -0700

    Update testpipeline.sh

--- scripts/testpipeline.sh
+++ scripts/testpipeline.sh
@@ -1,12 +1,16 @@
 
-oc delete project test-pipeline-demo
-cd /root/podmancamelsap 
+
+#https://github.com/jenkins-x/jx/issues/3767
+cd /root/podmancamelsap
 rm -rf openshift4gradle
 git clone https://github.com/marcredhat/openshift4gradle.git
+
+cd openshift4gradle
 rm -rf createpipeline.sh
 rm -rf rsync_to_container.sh
 wget https://raw.githubusercontent.com/marcredhat/crcdemos/master/scripts/rsync_to_container.sh
 wget https://raw.githubusercontent.com/marcredhat/crcdemos/master/scripts/createpipeline.sh
 chmod +x *.sh
+#edit createpipeline.sh with new project name
 ./createpipeline.sh
 ./rsync_to_container.sh
