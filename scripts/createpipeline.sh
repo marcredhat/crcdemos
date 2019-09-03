b80ec76da394cbf390273125eb9febb8f3747618
Marc Chisinevski <mchisine@redhat.com>
2019-09-03 11:53:35 -0700

    Update createpipeline.sh

--- scripts/createpipeline.sh
+++ scripts/createpipeline.sh
@@ -1,5 +1,18 @@
-oc project pipeline-demo
-oc delete pods --all
+oc new-project test-pipeline-demo
+
+# create service account
+
+oc create serviceaccount pipeline
+oc adm policy add-scc-to-user privileged -z pipeline
+oc adm policy add-role-to-user edit -z pipeline
+
+# create pipline tasks and resources
+
+oc create -f tasks
+oc create -f https://raw.githubusercontent.com/tektoncd/catalog/master/buildah/buildah.yaml
+oc create -f https://raw.githubusercontent.com/tektoncd/catalog/master/openshift-client/openshift-client-task.yaml
+oc create -f pipelines/mapit-resources.yml
+
 oc apply -f apps/mapit-spring.yml
 oc delete  -f pipelines/build-pipeline.yml
 oc delete -f pipelines/mapit-build-pipeline-run.yml
