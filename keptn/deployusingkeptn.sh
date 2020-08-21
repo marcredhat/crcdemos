b189931f708b0dae86c08b3f6ee33e4ee159fa1a
Marc Chisinevski <mchisine@redhat.com>
2020-08-21 15:27:12 -0700

    Update deployusingkeptn.sh

--- keptn/deployusingkeptn.sh
+++ keptn/deployusingkeptn.sh
@@ -1,4 +1,5 @@
 GIT_TOKEN=<your git token>
+KEPTN_PROJECT=marc-crc-keptn-3
 KEPTN_ENDPOINT=http://api-gateway-nginx-keptn.apps-crc.testing/api
 KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -ojsonpath={.data.keptn-api-token} | base64 --decode)
 echo $KEPTN_API_TOKEN
@@ -16,13 +17,35 @@ keptn status
 
 #cd examples/onboarding-carts/
 
-keptn create project marc-crc-keptn-2 --shipyard ./shipyard.yaml --git-user=marcredhat --git-token=$GIT_TOKEN \
---git-remote-url=https://github.com/marcredhat/marc-crc-keptn-2.git --suppress-websocket
+keptn create project marc-crc-keptn-3 --shipyard ./shipyard.yaml --git-user=marcredhat --git-token=$GIT_TOKEN \
+--git-remote-url=https://github.com/marcredhat/marc-crc-keptn-3.git
+#expected result:
+#https://github.com/marcredhat/marc-crc-keptn-3.git should have 4 branches as requested in the above shipyard.yaml
+#Starting to create project
+#ID of Keptn context: 9388cd2d-5d78-4c9a-9c78-c5ed293d9cce
+#Project marc-crc-keptn-3 created
+#Stage dev created
+#Stage staging created
+#Stage production created
+#Shipyard successfully processed
 
 sleep 10
 
-keptn onboard service carts --project=marc-crc-keptn-2 --chart=./carts
+keptn onboard service carts --project=marc-crc-keptn-3 --chart=./carts
+#expected result:
+#Starting to onboard service
+#ID of Keptn context: 5fba67c2-776e-4dcb-bd4c-e4a30bb39052
+#Create umbrella Helm Chart for project marc-crc-keptn-3
+#Creating new Keptn service carts in stage dev
+#Creating new Keptn service carts in stage staging
+#Service already exists
 
 sleep 10
 
-keptn send event new-artifact --project=marc-crc-keptn-2 --service=carts --image=docker.io/keptnexamples/carts --tag=0.11.2
+keptn send event new-artifact --project=marc-crc-keptn-3 --service=carts --image=docker.io/keptnexamples/carts --tag=0.11.2
+#expected result:
+#Starting to send a new-artifact-event to deploy the service carts in project marc-crc-keptn-3 in version docker.io/keptnexamples/carts:0.11.2
+#ID of Keptn context: f8bea429-3074-4a99-aeac-7999405ac84e
+#Start updating chart carts of stage dev
+#Finished updating chart carts of stage dev
+#Start upgrading chart marc-crc-keptn-3-dev-carts in namespace marc-crc-keptn-3-dev
