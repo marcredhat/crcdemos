9cac2aaa08478a57844419b06f3952363d1c7b48
Marc Chisinevski <mchisine@redhat.com>
2020-08-21 14:23:32 -0700

    Create configurekeptn.sh

---
+++ keptn/configurekeptn.sh
@@ -0,0 +1,14 @@
+oc expose svc api-gateway-nginx
+#oc get route
+#NAME                HOST/PORT                                  PATH   SERVICES            PORT   TERMINATION   WILDCARD
+#api-gateway-nginx   api-gateway-nginx-keptn.apps-crc.testing          api-gateway-nginx   http                 None
+
+export KEPTN_ENDPOINT=http://api-gateway-nginx-keptn.apps-crc.testing/api
+export KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -ojsonpath={.data.keptn-api-token} | base64 --decode)
+echo $KEPTN_API_TOKEN
+
+keptn auth --endpoint=$KEPTN_ENDPOINT --api-token=$KEPTN_API_TOKEN
+#Starting to authenticate
+#Successfully authenticated
+
+keptn configure bridge --output
