7f9937e4c72c25016a16f77346c0a13b5eae3737
Marc Chisinevski <mchisine@redhat.com>
2020-08-21 13:08:11 -0700

    Update cleanup.sh

--- keptn/cleanup.sh
+++ keptn/cleanup.sh
@@ -2,9 +2,12 @@ crc delete
 rm -rf istio
 #oc delete project marc-bookinfo
 #oc delete project istio-system
-#rm -rf mesh1.sh
-#rm -rf deploybookinfo.sh
+rm -rf ./mesh1.sh
+rm -rf ./deploybookinfo.sh
+rm -rf ./deploykeptn.sh
 curl -L https://raw.githubusercontent.com/marcredhat/crcdemos/master/keptn/deploybookinfo.sh -o deploybookinfo.sh
 curl -L https://raw.githubusercontent.com/marcredhat/crcdemos/master/keptn/mesh1.sh -o mesh1.sh
+curl -L https://raw.githubusercontent.com/marcredhat/crcdemos/master/keptn/deploykeptn.sh -o deploykeptn.sh
 sudo chmod +x ./deploybookinfo.sh
 sudo chmod +x ./mesh1.sh
+sudo chmod +x ./deploykeptn.sh
