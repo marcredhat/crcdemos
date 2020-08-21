35bc96dd3f584bd457e7a3e9c26a0a9faacb3c62
Marc Chisinevski <mchisine@redhat.com>
2020-08-21 13:19:34 -0700

    Update deploykeptn.sh

--- keptn/deploykeptn.sh
+++ keptn/deploykeptn.sh
@@ -7,3 +7,11 @@ sleep 30
 oc get pvc
 
 oc get pods
+
+sleep 60
+
+for deploy in `oc get deploy -n keptn |awk '{ print $1 }'`
+do
+  oc set resources deployment $deploy  --limits=cpu=10m,memory=128Mi --requests=cpu=10m,memory=64Mi
+  oc delete pods --all -n keptn
+done
