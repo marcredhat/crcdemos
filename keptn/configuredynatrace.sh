b073eab29eb5450b45c0056a06f6adf65810e847
Marc Chisinevski <mchisine@redhat.com>
2020-08-21 17:34:46 -0700

    Update configuredynatrace.sh

--- keptn/configuredynatrace.sh
+++ keptn/configuredynatrace.sh
@@ -1,4 +1,7 @@
+oc project keptn
+oc expose svc api-gateway-nginx
 #we'll use the route we create by exposing the api-gateway-nginx service.
+
 KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -ojsonpath={.data.keptn-api-token} | base64 --decode)
 DT_API_TOKEN=<Dynatrace API token>
 KEPTN_PROJECT=marc-crc-keptn-4
@@ -6,6 +9,6 @@ oc -n keptn create secret generic dynatrace --from-literal="DT_API_TOKEN=<Dynatr
       --from-literal="DT_TENANT=eye15053.live.dynatrace.com" \
       --from-literal="KEPTN_API_URL=http://api-gateway-nginx-keptn.apps-crc.testing/api" \
       --from-literal="KEPTN_API_TOKEN=<Keptn API token>" -o yaml --dry-run=client | oc apply -f -
-oc new-project dynatrace
+oc project dynatrace
 kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/dynatrace-service/0.8.0/deploy/service.yaml
 keptn configure monitoring dynatrace --project=$KEPTN_PROJECT --service=carts --suppress-websocket
