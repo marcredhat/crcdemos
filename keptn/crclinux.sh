60d2b2b58de4ee1e7ff2cb5f5e4c451bcb78ae51
Marc Chisinevski <mchisine@redhat.com>
2020-10-08 15:42:14 -0700

    Create crclinux.sh

---
+++ keptn/crclinux.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+START_TS=$(date +%s)
+SINV="${0} ${@}"
+SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+err() {
+    echo; echo;
+    echo -e "\e[97m\e[101m[ERROR]\e[0m ${1}"; shift; echo;
+    while [[ $# -gt 0 ]]; do echo "    $1"; shift; done
+    echo; exit 1;
+}
+
+# Checking if we are root
+#test "$(whoami)" = "root" || err "Not running as root"
+
+#get the latest CodeReady Containers version from https://github.com/code-ready/crc/branches
+#export CRCVERSION=1.17.0
+#export CRCMEM=128000
+#export CRCCPUS=128
+
+test -z "$CRCVERSION" && CRCVERSION="1.17.0"
+test -z "$CRCMEM" && CRCMEM="16000"
+test -z "$CRCCPUS" && CRCCPUS="8"
+
+tar -xvf crc-linux-amd64.tar.xz
+#export PATH=$PATH:`pwd`/crc-macos-$CRCVERSION-amd64/
+sudo cp `pwd`/crc-linux-$CRCVERSION-amd64/crc /usr/local/bin
+crc config set memory 16000
+#crc config set cpus 12
+crc setup
+crc start --pull-secret-file pullsecret.txt
+#eval $(crc oc-env)
+#oc version
+crc console
