811db2ded6e100988a2a174d8a31e33083fd1ffc
Marc Chisinevski <mchisine@redhat.com>
2020-10-08 19:33:01 -0700

    Update crclinux.sh

--- keptn/crclinux.sh
+++ keptn/crclinux.sh
@@ -21,17 +21,29 @@ err() {
 #export CRCMEM=128000
 #export CRCCPUS=128
 
-test -z "$CRCVERSION" && CRCVERSION="1.17.0"
+
+
+#test -z "$CRCVERSION" && CRCVERSION="1.17.0"
 test -z "$CRCMEM" && CRCMEM="16000"
 test -z "$CRCCPUS" && CRCCPUS="8"
 
+
+sudo yum -y install wget tar git podman buildah skopeo
+sudo rm -rf ./crc-linux-amd64.tar.xz
+wget https://mirror.openshift.com/pub/openshift-v4/clients/crc/latest/crc-linux-amd64.tar.xz
 tar -xvf crc-linux-amd64.tar.xz
 #export PATH=$PATH:`pwd`/crc-macos-$CRCVERSION-amd64/
+#sudo cp `pwd`/crc-linux-$CRCVERSION-amd64/crc /usr/local/bin
+export CRCVERSION=$(ls -latr `pwd` | awk FNR==2' {print $0}' | awk -F " " '{print $NF}' | awk  -F "-" '{print $3}')
 sudo cp `pwd`/crc-linux-$CRCVERSION-amd64/crc /usr/local/bin
-crc config set memory 16000
-#crc config set cpus 12
+
+crc config set memory $CRCMEM
+crc config set cpus $CRCCPUS
 crc setup
 crc start --pull-secret-file pullsecret.txt
-#eval $(crc oc-env)
-#oc version
+crc config get memory
+crc config get cpus
+eval $(crc oc-env)
+oc version
 crc console
+
